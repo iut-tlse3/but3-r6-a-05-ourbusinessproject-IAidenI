87014378bf7ed11d9198b0887dae97dcdc314d02
IAidenI <bolerichardnathan@gmail.com>
2026-01-27 11:46:16 +0100

    fix #1.3 Mapping Project et Enterprise

--- src/main/java/ourbusinessproject/Enterprise.java
+++ src/main/java/ourbusinessproject/Enterprise.java
@@ -1,18 +1,32 @@
 package ourbusinessproject;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 
+@Entity
+@Table(name = "enterprises")
 public class Enterprise {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
     @NotEmpty
+    @Column(name = "name", nullable = false)
     private String name;
+
     @Size(min=10)
+    @Column(name = "description")
     private String description;
+
     @NotEmpty
+    @Column(name = "contact_name", nullable = false)
     private String contactName;
+
     @NotEmpty
     @Email
+    @Column(name = "contact_email", nullable = false)
     private String contactEmail;
 
     Enterprise() {
--- src/main/java/ourbusinessproject/Project.java
+++ src/main/java/ourbusinessproject/Project.java
@@ -1,12 +1,21 @@
 package ourbusinessproject;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 
+@Entity
+@Table(name = "projects")
 public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
     @NotEmpty
-    String title;
-    String description;
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description")
+    private String description;
 
     Project() {
 
